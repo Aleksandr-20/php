b081ab702757a42c5e94de95f1354072b0e81955
ifmo-web <ifmo-web@gmail.com>
2022-01-18 20:29:58 +0300

    Куки (занятие 8)

---
+++ lesson8/cookie/colors.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="colors.php" method="post">
+    <label for="colors">Изменить цвет фона</label>
+    <select id="colors" name="colors">
+        <option value="#B3E5FC">Голубой</option>
+        <option value="#CE93D8">Сиреневый</option>
+        <option value="#F5F5F5">Серый</option>
+        <option value="#F8BBD0">Розовый</option>
+    </select>
+    <input type="submit" value="Изменить">
+</form>
+
+</body>
+</html>
